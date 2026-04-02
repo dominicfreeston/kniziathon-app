9c3aaef0bd2ced25a2886c035d5ea68bbc0f8f6f
Dominic <dominic.freeston@gmail.com>
2026-04-02 18:17:51 +0100

    [CLAUDE] Fix dev namespace

--- dev/user.clj
+++ dev/user.clj
@@ -5,10 +5,15 @@
             [ring.middleware.reload :refer [wrap-reload]]))
 
 (defonce server (atom nil))
+(defonce dev-state (delay (state/create-state state/default-data-file)))
+
+(defn dev-handler [request]
+  ((core/create-app @dev-state) request))
 
 (defn start! []
-  (state/load-state!)
-  (reset! server (run-jetty (wrap-reload #'core/app) {:port 3000 :join? false}))
+  (reset! server
+          (run-jetty (wrap-reload #'dev-handler)
+                     {:port 3000 :join? false}))
   (println "Dev server started on http://localhost:3000"))
 
 (defn stop! []
